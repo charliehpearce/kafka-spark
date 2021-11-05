9d8b5cdb2bab6a770460bcce50ec41a9886aca3e
Charles H J  Pearce <charleshjpearce@Charless-MacBook-Pro.local>
2021-11-05 16:28:25 +0000

    better spark processing

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,33 +1,54 @@
-from kafka import KafkaConsumer
-import pandas as pd
-import json
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import decode, from_json, col, explode
+from pyspark.sql.types import StructType, StructField, FloatType, TimestampType, StringType
 
 
 def main():
-    # Define kafka consumer
-    consumer = KafkaConsumer(
-        'prices',
-        bootstrap_servers='kafka:9092',
-        group_id='group1',
-        auto_offset_reset='earliest',
-        consumer_timeout_ms=10000,  # If no messages recived after 10s break the iterator
-        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-    )
     # Open spark session
     spark = SparkSession.builder.master('local').getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
+    spark.sparkContext.setLogLevel('ERROR')
 
+    """
+    consumer = KafkaConsumer(
+    'prices',
+    bootstrap_servers='kafka:9092',
+    group_id='group1',
+    auto_offset_reset='earliest',
+    consumer_timeout_ms=10000,  # If no messages recived after 10s break the iterator
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
     batch = []
     for msg in consumer:
         batch.append(msg.value)
         print(msg.value)
 
-    # Create Spark df
+    # Create Spark DF
     data = (tuple(i.values()) for i in batch)
     cols = list(batch[0].keys())
     df = spark.createDataFrame(data=data, schema=cols)
     df.show()
+    """
+
+    schema = StructType([
+        StructField('ts', StringType(), True),
+        StructField('symbol', StringType(), True),
+        StructField('price', FloatType(), True)
+    ])
+
+    input_stream = spark\
+        .read\
+        .format('kafka')\
+        .option('kafka.bootstrap.servers', 'kafka:9092')\
+        .option('kafka.group.id', 'group1')\
+        .option('subscribe', 'prices')\
+        .option('startingOffsets', 'earliest')\
+        .option('endingOffsets', 'latest')\
+        .load()\
+        .select(from_json(col("value").cast("string"), schema).alias("parsed_value"))
+
+    df = input_stream.select("parsed_value.*")
+
+    df.printSchema()
 
     # Summary stats
     df_summary = df.describe(['price'])
--- consumer/spark-submit.sh
+++ consumer/spark-submit.sh
@@ -1,1 +1,1 @@
-/usr/local/spark/bin/spark-submit --conf "-Dvm.logging.level=WARNING" --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4  consumer.py
+/usr/local/spark/bin/spark-submit --conf "-Dvm.logging.level=WARNING" --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0  consumer.py
