cd45e93f4ae93d5052282530733d9c1bf59f46e6
Charles H J  Pearce <charleshjpearce@Charless-MacBook-Pro.local>
2021-11-05 12:47:28 +0000

    working with spark

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 
 
 def main():
-    # Define consumer obj
+    # Define kafka consumer
     consumer = KafkaConsumer(
         'prices',
         bootstrap_servers='kafka:9092',
@@ -14,43 +14,33 @@ def main():
         consumer_timeout_ms=10000,  # If no messages recived after 10s break the iterator
         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
+    # Open spark session
+    spark = SparkSession.builder.master('local').getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
 
     batch = []
     for msg in consumer:
         batch.append(msg.value)
         print(msg.value)
 
-    # Open spark session
-    spark = SparkSession.builder.master('local').getOrCreate()
-
-    data = [tuple(i.values()) for i in batch]
+    # Create Spark df
+    data = (tuple(i.values()) for i in batch)
     cols = list(batch[0].keys())
-
     df = spark.createDataFrame(data=data, schema=cols)
-
     df.show()
 
-    # Calcualte stats
-    print(f'There are {len(batch)} objects in the topic "prices"')
-    df = pd.DataFrame(batch)
-    price_col = df['price']
-    max_value = price_col.max()
-    min_value = price_col.min()
-    mean_value = price_col.mean()
-    std_value = price_col.std()
+    # Summary stats
+    df_summary = df.describe(['price'])
+    df_summary.show()
 
-    print(
-        f'Max: {max_value}, min: {min_value}, mean: {mean_value}, std: {std_value}')
+    # Summary table into dictionary
+    stats = dict(
+        map(lambda row: (row.summary, float(row.price)), df_summary.collect()))
 
-    # Is outlier if the price is 3 std away from the mean
-    upper_bound = mean_value + 3*std_value
-    lower_bound = mean_value - 3*std_value
-
-    # Filter dataframe
-    outliers = df[(df['price'] > upper_bound) |
-                  (df['price'] < lower_bound)]
-
-    print(outliers.to_string(index=False))
+    # Filter by 3*price std and show dataframe
+    upper_bound = stats['mean'] + 3*stats['stddev']
+    lower_bound = stats['mean'] - 3*stats['stddev']
+    df.filter((df.price > upper_bound) | (df.price < lower_bound)).show()
 
 
 if __name__ == "__main__":
